44f046952f77c6d2257f4b214f7e627abf5b6cb7
Ling Rosenstein <greekyogurtgirl@gmail.com>
2014-06-25 00:19:12 -0400

    user can publish articles. Add a little decoration,still looks ugly though

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,19 @@
 class PostsController < ApplicationController
   before_action :require_current_user, only: [:new, :create]
   before_action :require_owner, only: [:edit, :update, :destroy]
-  
+
   def new
     @sub = Sub.find(params[:sub_id])
-    @pot = @sub.posts.new
+    @post = @sub.posts.new
+    @post.sub = @sub
   end
-  
+
   def create
     @sub = Sub.find(params[:sub_id])
     @post = @sub.posts.new(post_params)
+    @post.sub = @sub
     @post.user_id = current_user.id
-    
+
     if @post.save
       redirect_to post_url(@post)
     else
@@ -19,21 +21,29 @@ class PostsController < ApplicationController
       render :new
     end
   end
-  
+
   def index
-    
+    @sub = Sub.find(params[:sub_id])
+    @posts = @sub.posts
+    redirect_to @sub
   end
 
-  
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
-  
+
   def post_params
     params.require(:post).permit(:title, :body, :sub_id, :user_id)
   end
-  
+
   def require_owner
     @post = Post.find(params[:id])
-    
-    
+
+    unless @post.user_id == current_user.id || current_user == @post.sub.manager
+      flash[:errors] = ["Only submitter of this post can implement this action!"]
+      redirect_to post_url(@post)
+    end
   end
 end
--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,11 +1,11 @@
 class SubsController < ApplicationController
   before_action :require_owner, only: [ :destroy, :edit, :update]
-  before_action :require_current_user, except: [:index]
-  
+  before_action :require_current_user, only: [:new, :create]
+
   def new
     @sub = current_user.subs.new
   end
-  
+
   def create
     @sub = current_user.subs.new(sub_params)
     @sub.user_id = current_user.id
@@ -17,47 +17,43 @@ class SubsController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
     @sub = Sub.find(params[:id])
   end
-  
+
   def update
     @sub = Sub.find(params[:id])
     if @sub.update_attributes(sub_params)
-      redirect_to sub_url(sub)
+      redirect_to sub_url(@sub)
     else
       flash.now[:errors] = @sub.errors.full_messages
       render :edit
     end
   end
-  
+
   def show
- 
+
     @sub = Sub.find(params[:id])
-    if @sub
-      render :show
-    else
-      flash.now[:errors] = @sub.errors.full_messages
-      redirect_to subs_url
-    end
+
+    render :show
   end
-  
+
   def index
     @subs = Sub.all
   end
-  
+
   def destroy
     @sub = Sub.find(params[:id])
     @sub.destroy
     redirect_to subs_url
   end
-  
+
   private
   def sub_params
     params.require(:sub).permit(:title, :user_id)
   end
-  
+
   def require_owner
     @sub = Sub.find(params[:id])
     unless @sub.manager == current_user
