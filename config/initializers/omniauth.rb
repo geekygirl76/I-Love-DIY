4e0e8a24555c267eb74103050b0d7d6d62bdbfc0
App Academy Student <student@appacademy.io>
2014-06-27 11:36:08 -0400

    commit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['771887046166094'], ENV['b3a1f49c3d6620e3e89432f25f04f8bb']
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
 end
 
