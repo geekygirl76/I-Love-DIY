279da7f8e05809789e09ba3f205dad67587a4e15
Ling Rosenstein <greekyogurtgirl@gmail.com>
2014-06-29 23:48:56 -0400

    started jbackbone gem

---
+++ app/assets/javascripts/diy.js
@@ -0,0 +1,13 @@
+window.Diy = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Diy.initialize();
+});
