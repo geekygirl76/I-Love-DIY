9c629f7577d8eb8e7d4350e5d44de9a2043fc96e
App Academy Student <student@appacademy.io>
2014-07-08 12:06:50 -0400

    backbone create and edit post channel dropdown show subforum

--- app/assets/javascripts/views/channels/channel_show.js
+++ app/assets/javascripts/views/channels/channel_show.js
@@ -2,6 +2,7 @@ Diy.Views.ChannelShow = Backbone.View.extend({
   template: JST["channels/show"],
 
   render: function(){
+    console.log((Diy.subs.get(this.model.get("sub_id"))).escape("title"));
     var content = this.template({ channel: this.model, posts: Diy.posts });
     this.$el.html(content);
     return this;
