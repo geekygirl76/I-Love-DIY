de7a6e26a7aef665bbe4b666a0271e498687ca0a
Ling Rosenstein <greekyogurtgirl@gmail.com>
2014-07-12 10:54:57 -0400

    fixed nav bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -83,7 +83,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
+   
     if @post.update_attributes(post_params)
       redirect_to @post
     else
