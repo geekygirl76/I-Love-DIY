d80b7d34a28643afd23341ab8d487262a4e5521e
App Academy Student <student@appacademy.io>
2014-07-14 12:29:13 -0400

    made more pic as links

--- app/assets/javascripts/models/comment.js
+++ app/assets/javascripts/models/comment.js
@@ -1,11 +1,17 @@
 Diy.Models.Comment = Backbone.Model.extend({
-  
+
   toJSON: function(options) {
-  
+
     var attr = _.clone(this.attributes);
-  
+
     return { "comment":  attr};
-     
-    }
-  
+
+    },
+
+    comments: function () {
+        this._comments = this._comments ||
+          new Diy.Collections.Comments([], { parent_comment: this });
+         return this._comments;
+      }
+
 });
--- app/views/api/comments/_comment.json.jbuilder
+++ app/views/api/comments/_comment.json.jbuilder
@@ -1,2 +1,3 @@
-json.(comment, :id, :post_id, :body, :created_at, :updated_at, :post_id, :user)
+json.(comment, :id, :post_id, :body, :created_at, :updated_at, :post_id, :user, :child_comments
+)
 json.author_name comment.user.username
