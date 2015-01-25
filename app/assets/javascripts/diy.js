6cd4b3fb6a74786f137d1afb9b450af76a13b5a5
Ling Rosenstein <greekyogurtgirl@gmail.com>
2015-01-24 19:10:10 -0500

    add background page

--- app/assets/javascripts/diy.js
+++ app/assets/javascripts/diy.js
@@ -5,14 +5,15 @@ window.Diy = {
   Routers: {},
   initialize: function() {
     // alert('Hello from Backbone!');
+
     subsData = JSON.parse(($("#bootstrapped-data")).html()).subs;
     Diy.subs = new Diy.Collections.Subs(subsData);
-    
+
     channelsData = JSON.parse(($("#bootstrapped-data")).html()).channels;
-    
+
     Diy.channels = new Diy.Collections.Channels(channelsData);
-    
-   
+
+
     Diy.posts = new Diy.Collections.Posts();
 
     Diy.posts.fetch({
